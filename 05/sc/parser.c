4a66aebd9d15accfa9fcec04389466c2cc083552
NotFounds <m134_warrock_rpg7@yahoo.co.jp>
2017-05-16 15:59:59 +0900

    Rename

--- 05/sc/parser.c
+++ 05/sc/parser.c
@@ -3,16 +3,20 @@
 
 void parse_expression();
 
+int buf[BUFSIZ];
+int *ptr = buf;
+
 void parse_factor()
 {
     if (token == TOKEN_ID)
     {
-        gen_code("load_id", lexeme);
+        //gen_code("load_id", lexeme);
         get_token();
     }
     else if (token == TOKEN_NUM)
     {
-        gen_code("load_num", lexvalue);
+        //gen_code("load_num", lexvalue);
+        sscanf(lexvalue, "%d", ptr++);
         get_token();
     }
     else if (token == TOKEN_LPAR)
@@ -32,7 +36,7 @@ void parse_factor()
     {
         get_token();
         parse_factor();
-        gen_code("not", "-");
+        //gen_code("not", "-");
     }
     else
     {
@@ -42,6 +46,7 @@ void parse_factor()
 
 void parse_term()
 {
+    int a, b;
     parse_factor();
     while (1)
     {
@@ -49,19 +54,25 @@ void parse_term()
         {
             get_token();
             parse_factor();
-            gen_code("mul", "-");
+            //gen_code("mul", "-");
+            b = *(--ptr);
+            a = *(--ptr);
+            *ptr++ = a * b;
         }
         else if (token == TOKEN_SLASH)
         {
             get_token();
             parse_factor();
-            gen_code("div", "-");
+            //gen_code("div", "-");
+            b = *(--ptr);
+            a = *(--ptr);
+            *ptr++ = a / b;
         }
         else if (token == TOKEN_AND)
         {
             get_token();
             parse_factor();
-            gen_code("and", "-");
+            //gen_code("and", "-");
         }
         else
         {
@@ -72,6 +83,7 @@ void parse_term()
 
 void parse_polynomial()
 {
+    int a, b;
     if (token == TOKEN_PLUS)
     {
         get_token();
@@ -81,7 +93,9 @@ void parse_polynomial()
     {
         get_token();
         parse_term();
-        gen_code("neg", "-");
+        //gen_code("neg", "-");
+        a = *(--ptr);
+        *(ptr++) = a * -1;
     }
     else
     {
@@ -94,19 +108,25 @@ void parse_polynomial()
         {
             get_token();
             parse_term();
-            gen_code("add", "-");
+            //gen_code("add", "-");
+            b = *(--ptr);
+            a = *(--ptr);
+            *(ptr++) = a + b;
         }
         else if (token == TOKEN_MINUS)
         {
             get_token();
             parse_term();
-            gen_code("sub", "-");
+            //gen_code("sub", "-");
+            b = *(--ptr);
+            a = *(--ptr);
+            *(ptr++) = a - b;
         }
         else if (token == TOKEN_OR)
         {
             get_token();
             parse_term();
-            gen_code("or", "-");
+            //gen_code("or", "-");
         }
         else
         {
@@ -122,41 +142,43 @@ void parse_expression()
     {
         get_token();
         parse_polynomial();
-        gen_code("eq", "-");
+        //gen_code("eq", "-");
     }
     else if (token == TOKEN_NE)
     {
         get_token();
         parse_polynomial();
-        gen_code("ne", "-");
+        //gen_code("ne", "-");
     }
     else if (token == TOKEN_LT)
     {
         get_token();
         parse_polynomial();
-        gen_code("lt", "-");
+        //gen_code("lt", "-");
     }
     else if (token == TOKEN_LE)
     {
         get_token();
         parse_polynomial();
-        gen_code("le", "-");
+        //gen_code("le", "-");
     }
     else if (token == TOKEN_GT)
     {
         get_token();
         parse_polynomial();
-        gen_code("gt", "-");
+        //gen_code("gt", "-");
     }
     else if (token == TOKEN_GE)
     {
         get_token();
         parse_polynomial();
-        gen_code("ge", "-");
+        //gen_code("ge", "-");
     }
 }
 
 void parse()
 {
+    ptr = buf;
     parse_expression();
+    print_ans(*buf);
 }
